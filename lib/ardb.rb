e2470664cc5498dee5b5bc3475279532f66400f5
Kelly Redding <kelly@kellyredding.com>
2013-03-29 11:55:48 -0500

    configure/init pattern for setting up AR
    This applies the pattern and uses it to setup AR.  Use this interface
    to configure AR and validate the configs.  This will also serve as
    a handy lookup for AR configuration parameters.

--- lib/ardb.rb
+++ lib/ardb.rb
@@ -1,3 +1,40 @@
+require 'active_record'
+require 'ns-options'
+require 'pathname'
+
 require "ardb/version"
 
-module Ardb; end
+module Ardb
+
+  def self.config; Config; end
+  def self.configure(&block); Config.define(&block); end
+
+  def self.init
+    if !self.config.required_set?
+      raise RuntimeError, "Missing required configuration values"
+    end
+    ActiveRecord::Base.establish_connection(self.config.db.to_hash)
+  end
+
+  class Config
+    include NsOptions::Proxy
+
+    namespace :db do
+      option :adapter,  String, :required => true
+      option :encoding, String, :required => false
+      option :url,      String, :required => true
+      option :database, String, :required => true
+      option :username, String, :required => true
+      option :password, String, :required => false
+    end
+
+    option :root_path,       Pathname, :required => true
+    option :migrations_path, String,   :default => proc{ default_migrations_path }
+    option :schema_path,     String,   :default => proc{ default_schema_path }
+
+    def self.default_migrations_path; root_path.join("db/migrations"); end
+    def self.default_schema_path;     root_path.join("db/schema.rb");  end
+
+  end
+
+end
---
+++ test/unit/config_tests.rb
@@ -0,0 +1,41 @@
+require 'assert'
+require 'ns-options/assert_macros'
+require 'ardb'
+
+class Ardb::Config
+
+  class BaseTests < Assert::Context
+    include NsOptions::AssertMacros
+    desc "Ardb::Config"
+    subject{ Ardb::Config }
+
+    should have_namespace :db
+    should have_option  :root_path, Pathname, :required => true
+    should have_options :migrations_path, :schema_path
+
+    should "should use `db/migrations` as the default migrations path" do
+      exp_path = Pathname.new(ROOT_PATH).join("db/migrations").to_s
+      assert_equal exp_path, subject.migrations_path
+    end
+
+    should "should use `db/schema.rb` as the default schema path" do
+      exp_path = Pathname.new(ROOT_PATH).join("db/schema.rb").to_s
+      assert_equal exp_path, subject.schema_path
+    end
+
+  end
+
+  class DbTests < BaseTests
+    desc "db namespace"
+    subject{ Ardb::Config.db }
+
+    should have_option :adapter,  String, :required => true
+    should have_option :encoding, String, :required => false
+    should have_option :url,      String, :required => true
+    should have_option :database, String, :required => true
+    should have_option :username, String, :required => true
+    should have_option :password, String, :required => false
+
+  end
+
+end
