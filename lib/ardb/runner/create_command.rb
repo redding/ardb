03de249febcb18636e590d99647f08884d4348bb
Kelly Redding <kelly@kellyredding.com>
2013-03-30 13:06:49 -0500

    create subcommand
    This adds a cli subcommand to create a database based on the config.
    This also reworks the test helper to do some configuration b/c this
    command requires a bit of configuration to be in place.
    
    This is only the start and this needs further work at some point.
    Some system tests would be nice and it would be good to expand it
    to support mysql/mysql2 and sqlite3.
    
    Closes #7.

---
+++ lib/ardb/runner/create_command.rb
@@ -0,0 +1,39 @@
+require 'active_record'
+require 'ardb/runner'
+
+# Note: currently only postgresql adapter supported
+
+class Ardb::Runner::CreateCommand
+
+  def run
+    begin
+      self.send("#{Ardb.config.db.adapter}_cmd").run
+    rescue Exception => e
+      $stderr.puts e, *(e.backtrace)
+      $stderr.puts "Couldn't create database for #{Ardb.config.db.inspect}"
+    end
+  end
+
+  def postgresql_cmd
+    PostgresqlCommand.new.run
+  end
+
+  class PostgresqlCommand
+    attr_reader :config_settings, :database
+
+    def initialize
+      @config_settings  = Ardb.config.db.to_hash
+      @database = Ardb.config.db.database
+    end
+
+    def run
+      ActiveRecord::Base.establish_connection(@config_settings.merge({
+        :database           => 'postgres',
+        :schema_search_path => 'public'
+      }))
+      ActiveRecord::Base.connection.create_database(@database, @config_settings)
+      ActiveRecord::Base.establish_connection(@config_settings)
+    end
+  end
+
+end
