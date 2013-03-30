72b40f617eb33a3a0984fe46b334f649705f27c5
Kelly Redding <kelly@kellyredding.com>
2013-03-30 14:41:32 -0500

    generate subcommand and migration implementation
    This adds a generate subcommand and implements generating migrations.
    
    Closes #6.

---
+++ lib/ardb/runner/generate_command.rb
@@ -0,0 +1,47 @@
+require 'active_support/core_ext/string/inflections'
+require 'ardb/runner'
+
+# Note: currently only postgresql adapter supported
+
+class Ardb::Runner::GenerateCommand
+
+  def initialize(args)
+    @item = args.shift
+    @args = args
+  end
+
+  def run
+    begin
+      self.send("#{@item}_cmd").run
+    rescue Exception => e
+      $stderr.puts e, *(e.backtrace)
+      $stderr.puts "Couldn't generate #{@item}."
+    end
+  end
+
+  def migration_cmd
+    MigrationCommand.new(@args.first).run
+  end
+
+  class MigrationCommand
+    attr_reader :identifier, :class_name, :file_name, :template
+
+    def initialize(identifier)
+      @identifier = identifier
+      @class_name = @identifier.classify.pluralize
+      @file_name  = begin
+        "#{Time.now.strftime("%Y%m%d%H%M%S")}_#{@identifier.underscore}"
+      end
+      @template = "class #{@class_name} < ActiveRecord::Migration\n"\
+                  "  def change\n"\
+                  "  end\n"\
+                  "end\n"
+    end
+
+    def run
+      file_path = File.join(Ardb.config.migrations_path, "#{@file_name}.rb")
+      File.open(file_path, "w"){ |f| f.write(@template) }
+    end
+  end
+
+end
