e464d02119afbe5f6310dadff0283cedac78b753
Patrick Wilkin <patrick.wilkin@reelfx.com>
2015-12-03 11:46:33 -0600

    Move the logic for Migrating a DB into Adapter::Base
    This moves the logic for migrating a DB into Adapter::Base so that
    it may be called independently of the command line.  Though the migration
    doesn't directly use the Adapter, it is a common place for it to live
    so that all Adapters can migrate the db in a similar manner as they
    create or drop the db.
    
    This also updates the TestHelpers so that apps do not need to directly
    interact with the Adapter to drop, create, migrate, or reset the db.
    This used the existing pattern of only running a DB command once per
    call, but still allowing users to force the call using the bang version
    of the method.

--- lib/ardb/test_helpers.rb
+++ lib/ardb/test_helpers.rb
@@ -16,9 +16,42 @@ module Ardb::TestHelpers
     Ardb.adapter.load_schema
   end
 
-  def reset_db!
-    Ardb.adapter.drop_db
+  def create_db!
     Ardb.adapter.create_db
+  end
+
+  def create_db
+    @create_db ||= begin
+      self.create_db!
+      true
+    end
+  end
+
+  def drop_db!
+    Ardb.adapter.drop_db
+  end
+
+  def drop_db
+    @drop_db ||= begin
+      self.drop_db!
+      true
+    end
+  end
+
+  def migrate_db!
+    Ardb.adapter.migrate_db
+  end
+
+  def migrate_db
+    @migrate_db ||= begin
+      self.migrate_db!
+      true
+    end
+  end
+
+  def reset_db!
+    self.drop_db!
+    self.create_db!
     self.load_schema
   end
 
--- test/unit/runner/migrate_command_tests.rb
+++ test/unit/runner/migrate_command_tests.rb
@@ -10,51 +10,7 @@ class Ardb::Runner::MigrateCommand
     end
     subject{ @cmd }
 
-    should have_readers :migrations_path, :version, :verbose
-
-    should "use the config's migrations path" do
-      assert_equal Ardb.config.migrations_path, subject.migrations_path
-    end
-
-    should "not target a specific version by default" do
-      assert_nil subject.version
-    end
-
-    should "be verbose by default" do
-      assert subject.verbose
-    end
-
-  end
-
-  class VersionTests < UnitTests
-    desc "with a version ENV setting"
-    setup do
-      ENV["VERSION"] = '12345'
-      @cmd = Ardb::Runner::MigrateCommand.new
-    end
-    teardown do
-      ENV["VERSION"] = nil
-    end
-
-    should "should target the given version" do
-      assert_equal 12345, subject.version
-    end
-
-  end
-
-  class VerboseTests < UnitTests
-    desc "with a verbose ENV setting"
-    setup do
-      ENV["VERBOSE"] = 'no'
-      @cmd = Ardb::Runner::MigrateCommand.new
-    end
-    teardown do
-      ENV["VERBOSE"] = nil
-    end
-
-    should "turn off verbose mode if not set to 'true'" do
-      assert_not subject.verbose
-    end
+    should have_imeths :run
 
   end
 
